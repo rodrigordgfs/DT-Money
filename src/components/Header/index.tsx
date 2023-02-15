84d842bd244d0bba71ff65f19287524042742bb6
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-02-15 14:51:48 -0300

    Feature: Criando um modal acessível

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
+import { X } from "phosphor-react";
 import Logo from "../../assets/Logo.svg";
 
 export function Header() {
@@ -6,7 +8,20 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={Logo} />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Close>
+                <X />
+              </Dialog.Close>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
