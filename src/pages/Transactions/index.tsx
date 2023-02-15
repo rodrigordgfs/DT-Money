df473d8c4d483cb6d6d4985cbb11b57dc3c77c36
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-02-14 23:55:31 -0300

    Feature: Tabela de Transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,58 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>20/02/2021</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>20/02/2021</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>20/02/2021</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>20/02/2021</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </>
   );
 }
