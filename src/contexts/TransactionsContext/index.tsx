95053e77d841550e7c1ab6d78801de7e8da2b313
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-02-15 19:09:16 -0300

    Feature: Buscando transações

--- src/contexts/TransactionsContext/index.tsx
+++ src/contexts/TransactionsContext/index.tsx
@@ -3,6 +3,7 @@ import { ITransaction } from "../../interfaces/ITransaction";
 
 interface ITransactionContextType {
   transactions: ITransaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 
 interface ItransactionProviderProps {
@@ -14,16 +15,22 @@ export const TransactionsContext = createContext({} as ITransactionContextType);
 export function TransactionsProvider({ children }: ItransactionProviderProps) {
   const [transactions, setTransactions] = useState<ITransaction[]>([]);
 
+  async function fetchTransactions(query?: string) {
+    const url = new URL("http://localhost:3000/transactions");
+    if (query) {
+      url.searchParams.append("q", query);
+    }
+    const response = await fetch(url);
+    const data = await response.json();
+    setTransactions(data);
+  }
+
   useEffect(() => {
-    fetch("http://localhost:3000/transactions")
-      .then((response) => response.json())
-      .then((data) => {
-        setTransactions(data);
-      });
+    fetchTransactions();
   }, []);
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
       {children}
     </TransactionsContext.Provider>
   );
